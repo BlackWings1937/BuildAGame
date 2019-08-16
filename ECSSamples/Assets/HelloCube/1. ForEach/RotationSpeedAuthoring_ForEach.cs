cb720261c3421c55144ba5e31f391927668b6e95
BlackWings1937 <36805728+BlackWings1937@users.noreply.github.com>
2019-08-16 20:38:00 +0800

    1.增加ecs工程

---
+++ ECSSamples/Assets/HelloCube/1. ForEach/RotationSpeedAuthoring_ForEach.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+// ReSharper disable once InconsistentNaming
+[RequiresEntityConversion]
+public class RotationSpeedAuthoring_ForEach : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float DegreesPerSecond;
+
+    // The MonoBehaviour data is converted to ComponentData on the entity.
+    // We are specifically transforming from a good editor representation of the data (Represented in degrees)
+    // To a good runtime representation (Represented in radians)
+    // 这个文件用于桥接Monobehavivor和component，在这里将mono 中的数据用Component的形式转存。
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var data = new RotationSpeed_ForEach { RadiansPerSecond = math.radians(DegreesPerSecond) };
+        dstManager.AddComponentData(entity, data);
+    }
+}
---
+++ ECSSamples/Assets/HelloCube/2. IJobForEach/RotationSpeedSystem_IJobForEach.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+// This system updates all entities in the scene with both a RotationSpeed_IJobForEach and Rotation component.
+// 使用每一个实体的RS和R 跟新每一个实体
+
+// ReSharper disable once InconsistentNaming
+public class RotationSpeedSystem_IJobForEach : JobComponentSystem
+{
+    // Use the [BurstCompile] attribute to compile a job with Burst. You may see significant speed ups, so try it!
+    [BurstCompile]//                      在这里设置系统要操作的组件
+    struct RotationSpeedJob : IJobForEach<Rotation, RotationSpeed_IJobForEach>
+    {
+        public float DeltaTime;
+
+        // 这个方法只修改R组件，只读RS组件
+        // The [ReadOnly] attribute tells the job scheduler that this job will not write to rotSpeedIJobForEach
+        public void Execute(ref Rotation rotation, [ReadOnly] ref RotationSpeed_IJobForEach rotSpeedIJobForEach)
+        {
+            // Rotate something about its up vector at the speed given by RotationSpeed_IJobForEach.
+            rotation.Value = math.mul(math.normalize(rotation.Value), quaternion.AxisAngle(math.up(), rotSpeedIJobForEach.RadiansPerSecond * DeltaTime));
+        }
+    }
+
+    // OnUpdate runs on the main thread.
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        //相比起来应该叫做封了一包逻辑的数据，只是操作位置在这个文件而已
+        var job = new RotationSpeedJob
+        {
+            DeltaTime = Time.deltaTime
+        };
+
+        return job.Schedule(this, inputDependencies);
+    }
+}
