37adfdd330a77ebc44a5cc71866336a436e23417
774851951@qq.com <774851951@qq.com>
2019-11-29 11:57:53 +0800

    add git to manager file

---
+++ Linker/Assets/Scripts/Tools/GitFilesManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FileChange {
+    public string FileName;
+    public string FileAbsolutePath;
+    public EnumChangeType ChangeType = EnumChangeType.E_NONE;
+    public enum EnumChangeType {
+        E_NONE,
+        E_CHANGE,
+        E_DELETE,
+        E_ADD   
+    }
+}
+
+public class GitFilesManager : MonoBehaviour
+{
+
+    // ----- 对外接口 -----
+    public void StartSurveillanceFloder() {
+
+    }
+
+    public void StopSurveillanceFloder() {
+
+    }
+
+    public void UpdateFloderStatusAsNew() {
+
+    }
+
+    public List<FileChange> GetChangeFilesList() {
+        return null;
+    }
+
+    public void DeleteGitAtFloder() {
+
+    }
+
+
+    // ----- 私有方法 -----
+
+    private bool checkFloderExist(string floderPath) {
+        return Directory.Exists(floderPath);
+    }
+
+    private bool checkGitExist() {
+
+    }
+
+    private bool checkGitIsClean() { }
+
+}
---
+++ Linker/Assets/Scripts/Tools/GitUtil.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GitUtil
+{
+    public const string STR_NAME_GIT_PROJ_NAME = "LinkerData";
+
+    public const string STR_GIT_LOCAL = "";
+
+    public static bool CreateGitAtFloder(string path) {
+        path = path + STR_NAME_GIT_PROJ_NAME + "/";
+
+        Win32Util.RunCmd("cd "+path);
+        Win32Util.RunCmd("git clone "+STR_GIT_LOCAL);
+        return false;
+    }
+
+    public static bool CheckGitExistAtFloder(string path) {
+        path = path + STR_NAME_GIT_PROJ_NAME+"/";
+        if (!Directory.Exists(path)) {
+            return false;
+        }
+
+        Win32Util.RunCmd("cd " + path);
+        string result = Win32Util.RunCmd("git status");
+        if (result.Contains("fatal: not a git repository (or any of the parent directories): .git")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static bool CheckGitWorkTreeCleanAtFloder(string path) {
+        return false;
+    }
+
+
+    public static string GetGitCacheAreaPath(string path) {
+        return path + STR_NAME_GIT_PROJ_NAME + "/";
+    }
+}
